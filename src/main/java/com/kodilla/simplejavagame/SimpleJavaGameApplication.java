bd50281142120ac3fe051df63efee6cf2976b99f
Meham85 <codedtp@gmial.com>
2019-07-09 21:30:06 +0200

    pierwszy projekt

---
+++ src/main/java/com/kodilla/simplejavagame/SimpleJavaGameApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.simplejavagame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleJavaGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleJavaGameApplication.class, args);
+    }
+
+}
