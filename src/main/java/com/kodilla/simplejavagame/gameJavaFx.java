36c0a64dc7ff4410f6b2339a4868effcff03fa9d
Meham85 <codedtp@gmial.com>
2019-08-18 19:51:30 +0200

    Final version simple memo JavaFx Game

--- src/main/java/com/kodilla/simplejavagame/gameJavaFx.java
+++ src/main/java/com/kodilla/simplejavagame/gameJavaFx.java
@@ -1,140 +1,127 @@
 package com.kodilla.simplejavagame;
 
+import javafx.animation.Animation;
+import javafx.animation.Interpolator;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.animation.TimelineBuilder;
 import javafx.application.Application;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
+import javafx.scene.GroupBuilder;
 import javafx.scene.Scene;
+import javafx.scene.SceneBuilder;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.ButtonBuilder;
+import javafx.scene.layout.HBoxBuilder;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.LineBuilder;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class gameJavaFx extends Application {
-    public int P1_Score = 0;
-    public int P2_Score = 0;
+    DoubleProperty startXVal = new SimpleDoubleProperty(100.0);
+
+    Button startButton;
+    Button pauseButton;
+    Button resumeButton;
+    Button stopButton;
+    Line line;
+
+    Timeline anim = TimelineBuilder.create()
+            .autoReverse(true)
+            .keyFrames(
+                    new KeyFrame(
+                            new Duration(0.0),
+                            new KeyValue(startXVal, 100.0)
+                    ),
+                    new KeyFrame(
+                            new Duration(1000.0),
+                            new KeyValue(startXVal, 300.0, Interpolator.LINEAR)
+                    )
+            )
+            .cycleCount(Timeline.INDEFINITE)
+            .build();
 
     public static void main(String[] args) {
-        launch(args);
+        Application.launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage stage) {
+        Scene scene  = SceneBuilder.create()
+                .width(400)
+                .height(500)
+                .root(
+                        GroupBuilder.create()
+                                .children(
+                                        line = LineBuilder.create()
+                                                .startY(50)
+                                                .endX(200)
+                                                .endY(400)
+                                                .strokeWidth(4)
+                                                .stroke(Color.BLUE)
+                                                .build(),
+                                        HBoxBuilder.create()
+                                                .layoutX(60)
+                                                .layoutY(420)
+                                                .spacing(10)
+                                                .children(
+                                                        startButton = ButtonBuilder.create()
+                                                                .text("Start")
+                                                                .onAction(new EventHandler<ActionEvent>() {
+                                                                    @Override public void handle(ActionEvent e) {
+                                                                        anim.playFromStart();
+                                                                    }
+                                                                })
+                                                                .build(),
+                                                        pauseButton = ButtonBuilder.create()
+                                                                .text("Pause")
+                                                                .onAction(new EventHandler<ActionEvent>() {
+                                                                    @Override public void handle(ActionEvent e) {
+                                                                        anim.pause();
+                                                                    }
+                                                                })
+                                                                .build(),
+                                                        resumeButton = ButtonBuilder.create()
+                                                                .text("Resume")
+                                                                .onAction(new EventHandler<ActionEvent>() {
+                                                                    @Override public void handle(ActionEvent e) {
+                                                                        anim.play();
+                                                                    }
+                                                                })
+                                                                .build(),
+                                                        stopButton = ButtonBuilder.create()
+                                                                .text("Stop")
+                                                                .onAction(new EventHandler<ActionEvent>() {
+                                                                    @Override public void handle(ActionEvent e) {
+                                                                        anim.stop();
+                                                                    }
+                                                                })
+                                                                .build()
+                                                )
+                                                .build()
+                                )
+                                .build()
+                )
+                .build();
 
-        // vertical box
-        VBox vertBox = new VBox();
-        vertBox.setPrefSize(400, 400);
-        vertBox.setAlignment(Pos.CENTER);
+        line.startXProperty().bind(startXVal);
+        startButton.disableProperty().bind(anim.statusProperty()
+                .isNotEqualTo(Animation.Status.STOPPED));
+        pauseButton.disableProperty().bind(anim.statusProperty()
+                .isNotEqualTo(Animation.Status.RUNNING));
+        resumeButton.disableProperty().bind(anim.statusProperty()
+                .isNotEqualTo(Animation.Status.PAUSED));
+        stopButton.disableProperty().bind(anim.statusProperty()
+                .isEqualTo(Animation.Status.STOPPED));
 
-        // horizontal box for the players
-        HBox PlayerName = new HBox();
-        PlayerName.setAlignment(Pos.CENTER);
-        PlayerName.setPadding(new Insets(25, 25, 25, 25));
-
-        // horizontal box for the scores
-        HBox Score = new HBox();
-        Score.setAlignment(Pos.CENTER);
-
-        // horizontal box to show a goal
-        HBox Goal = new HBox();
-        Goal.setAlignment(Pos.CENTER);
-
-        // player1 name
-        Label team1 = new Label("Player 1");
-        team1.setTextFill(Color.RED);
-        team1.setAlignment(Pos.TOP_LEFT);
-        team1.setPadding(new Insets(25, 25, 25, 25));
-
-        // player2 name
-        Label team2 = new Label("Player 2");
-        team2.setTextFill(Color.BLUE);
-        team2.setAlignment(Pos.TOP_RIGHT);
-        team2.setPadding(new Insets(25, 25, 25, 25));
-
-        // player1 score
-        Label text1 = new Label();
-        text1.setAlignment(Pos.CENTER_LEFT);
-        text1.setText(Integer.toString(P1_Score));
-        text1.setPadding(new Insets(25, 30, 50, 25));
-
-        // player2 score
-        Label text2 = new Label();
-        text2.setAlignment(Pos.CENTER_RIGHT);
-        text2.setText(Integer.toString(P2_Score));
-        text2.setPadding(new Insets(25, 30, 50, 25));
-
-        // player1 goal
-        Button btn1 = new Button("Player 1 score");
-        btn1.setAlignment(Pos.BOTTOM_LEFT);
-        btn1.setPadding(new Insets(10, 10, 10, 10));
-
-        // player2 goal
-        Button btn2 = new Button("Player 2 score");
-        btn2.setPadding(new Insets(10, 10, 10, 10));
-        btn2.setAlignment(Pos.BOTTOM_RIGHT);
-
-        // event for player1 goal
-        btn1.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent arg0) {
-                // increase score
-                P1_Score = P1_Score + 1;
-                // display player1 score
-                text1.setText(Integer.toString(P1_Score));
-                if (P1_Score == 3) {
-                    resetScore(text1, text2);
-                }
-
-            }
-
-        });
-
-        // event for player2 goal
-        btn2.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent arg0) {
-                // increase player2 score
-                P2_Score = P2_Score + 1;
-                // display score
-                text2.setText(Integer.toString(P2_Score));
-
-                if (P2_Score == 3) {
-                    resetScore(text1, text2);
-
-                }
-            }
-
-
-        });
-
-        PlayerName.getChildren().addAll(team1, team2);
-        Score.getChildren().addAll(text1, text2);
-        Goal.getChildren().addAll(btn1, btn2);
-
-        vertBox.getChildren().addAll(PlayerName, Score, Goal);
-        primaryStage.setTitle("Counter");
-
-        Scene scene = new Scene(vertBox);
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
-
-    private void resetScore(Label text1, Label text2) {
-
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        P1_Score = 0;
-        P2_Score = 0;
-        text1.setText(Integer.toString(P1_Score));
-        text2.setText(Integer.toString(P2_Score));
-
-
+        stage.setScene(scene);
+        stage.setTitle("Metronome 1");
+        stage.show();
     }
 }
--- src/main/java/com/kodilla/simplejavagame/memoAppJavaFx.java
+++ src/main/java/com/kodilla/simplejavagame/memoAppJavaFx.java
@@ -3,12 +3,22 @@ package com.kodilla.simplejavagame;
 
 
 import java.util.*;
+import java.util.concurrent.Callable;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.beans.InvalidationListener;
 import javafx.beans.Observable;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.animation.FadeTransition;
 
@@ -17,7 +27,6 @@ import javafx.beans.property.BooleanProperty;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 
-import javafx.scene.control.ToggleButton;
 import javafx.scene.input.MouseEvent;
         import javafx.scene.layout.Pane;
         import javafx.scene.paint.Color;
@@ -30,44 +39,150 @@ import javafx.util.Duration;
 
 public class memoAppJavaFx extends Application {
 
-    private static final int Liczba_Par = 8;
-    private static final int Ilosc_Par_W_Rzedzie = 4;
+    private static final int Liczba_Par = 15;
+    private static final int Ilosc_Par_W_Rzedzie = 5;
 
     private Dachowka selected = null;
     private int clickCount = 2;
     private int clickNumber = 0;
     public int score;
+    public int scoreTemp;
+    public boolean gameActive = false;
+    private static final Integer STARTTIME = 0;
+    private Timeline timeline;
+    private Label timerLabel = new Label();
+    private IntegerProperty timeSeconds = new SimpleIntegerProperty(STARTTIME);
+    private double startTime;
+    private  double stopTempTime;
+    private double stopTime;
+    private double timeResult;
+    private double timeResultP1;
+    private double timeResultP2;
+    private double timeTemp;
+    public int scoreP1;
+    public int scoreP2;
+    private int activePlayer;
+    private String playerNumber;
 
 
-    TextField pole1 = new TextField("Punkty gracza " + score);
+    private TextField pole1 = new TextField("Punkty gracza 1 = " + String.valueOf(score));
+    private TextField pole2 = new TextField("Punkty gracza 2 = " + String.valueOf(score));
+    private TextField pole3 = new TextField("Czas gracza 1 - " + String.valueOf(timeResult));
+    private TextField pole4 = new TextField("Czas gracza 2 - " + String.valueOf(timeResult));
+    private TextField timeResultText = new TextField("Twój czas to " + String.valueOf(timeResult) + "s");
+    TextField punkty = new TextField();
+    TextField czas = new TextField();
+
+
+
 
     private Parent createContent() {
         Pane root = new Pane(); /* JavaFX 9. Tworzenie graficznych interfejsów użytkownika str314 */
         root.setPrefSize(400, 400);
 
-        ToggleButton timmerButton = new ToggleButton("Start gry");
-        BooleanProperty sel = timmerButton.selectedProperty(); /*Pobieramy właściwość selected. */
-        sel.addListener((observable, oldValue, newValue) -> {
-            System.out.println("Aktualny stan to: " + newValue);
-            timmerButton.setText(newValue ? "Gra w toku" : "Wznów grę");
-        });
-        timmerButton.relocate(20, 350);
-        root.getChildren().add(timmerButton); // Dodajemy przełącznik do rozkładu.
+        Button p1startButton = new Button("Start gracz 1");
+        Button p2startButton = new Button("Start gracz 2");
+
+        dachowkaCreator(root);
 
 
-        pole1.relocate(180, 350);
+
+        pole1.relocate(250, 330);
+        pole1.setEditable(false);
         root.getChildren().add(pole1);
-        pole1.textProperty().addListener(new InvalidationListener() {
 
-            @Override
-            public void invalidated(Observable observable) {
+        pole2.relocate(250, 360);
+        pole2.setEditable(false);
+        root.getChildren().add(pole2);
 
-                pole1.setText(String.valueOf(score));
-                System.out.println(score);
+        pole3.relocate(250, 260);
+        pole3.setEditable(false);
+        root.getChildren().add(pole3);
+
+        pole4.relocate(250, 300);
+        pole4.setEditable(false);
+        root.getChildren().add(pole4);
+
+
+
+        timerLabel.textProperty().bind(timeSeconds.asString());
+        timerLabel.setTextFill(Color.RED);
+        timerLabel.setStyle("-fx-font-size: 4em;");
+
+
+        p1startButton.setOnAction(new EventHandler<ActionEvent>() {
+
+            public void handle(ActionEvent event) {
+
+                if (timeline != null) {
+                    timeline.stop();
+
+                }
+                timeResult = -1; /* oznacza że nie zmieścił się w limicie */
+                gameActive = true;
+                activePlayer = 1;
+
+                System.out.println(activePlayer);
+                startGame();
+
+
             }
         });
 
+        p2startButton.setOnAction(new EventHandler<ActionEvent>() {
 
+            public void handle(ActionEvent event) {
+
+                if (timeline != null) {
+                    timeline.stop();
+
+                }
+                score = 0;
+                timeResult = -1;
+                gameActive = true;
+                activePlayer = 2;
+                System.out.println(activePlayer);
+                ArrayList<Dachowka> dachowki = new ArrayList<>();
+                System.out.println(activePlayer);
+                dachowkaDestroyer(root, dachowki);
+                dachowkaCreator(root);
+
+                startGame();
+
+                }
+                }
+
+
+
+        );
+
+
+        p1startButton.relocate(30, 350);
+        p2startButton.relocate(120, 350);
+        TextField timerText = new TextField("Walcz z czasem!");
+        timerText.relocate(270, 10);
+        timerText.setEditable(false);
+
+
+        VBox vb = new VBox(20);             // gap between components is 20
+
+
+        vb.relocate(300, 30);
+        vb.getChildren().addAll(timerLabel);
+        vb.setLayoutY(30);
+        timeResultText.relocate(250, 180);
+        root.getChildren().add(p1startButton);
+        root.getChildren().add(p2startButton);
+        root.getChildren().add(timerText);
+        root.getChildren().add(timeResultText);
+        root.getChildren().add(vb); // Dodajemy przełącznik do rozkładu.
+        return root;
+
+    }
+
+
+
+    public List<Dachowka> dachowkaCreator(Pane root) {
 
 
 
@@ -88,10 +203,29 @@ public class memoAppJavaFx extends Application {
             dachowka.setTranslateY(50 * (i / Ilosc_Par_W_Rzedzie)); /*przesunięcie w oparciu o ilość par */
             root.getChildren().add(dachowka);
         }
+        return dachowki;
+    }
+    public void dachowkaDestroyer (Pane root, ArrayList<Dachowka> dachowkaArrayList) {
+        for (Dachowka element: dachowkaArrayList
+        ) {root.getChildren().remove(element);
 
-        return root;
+        }
 
     }
+    public void startGame(){
+
+        startTime = System.nanoTime();
+
+        System.out.println(startTime);
+        timeSeconds.set(STARTTIME);
+        timeline = new Timeline();
+        timeline.getKeyFrames().add(
+                new KeyFrame(Duration.seconds(STARTTIME+1000),
+                        new KeyValue(timeSeconds, 1000)));
+        timeline.playFromStart();
+    }
+
+
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -103,10 +237,28 @@ public class memoAppJavaFx extends Application {
         primaryStage.show();
     }
 
-    private class Dachowka extends StackPane { /* JavaFX 9. Tworzenie graficznych interfejsów użytkownika str316 */
+    public class Dachowka extends StackPane { /* JavaFX 9. Tworzenie graficznych interfejsów użytkownika str316 */
         private Text text = new Text();
 
+
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            Dachowka dachowka = (Dachowka) o;
+
+            return text != null ? text.equals(dachowka.text) : dachowka.text == null;
+        }
+
+        @Override
+        public int hashCode() {
+            return text != null ? text.hashCode() : 0;
+        }
+
         public Dachowka(String value) {
+
             Circle border = new Circle(25, null);
             border.setFill(Color.WHITESMOKE);
             border.setStroke(Color.BLACK);
@@ -120,31 +272,135 @@ public class memoAppJavaFx extends Application {
             setOnMouseClicked(this::handleMouseClick);
             close();
         }
+        private void showAlert() {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Koniec gry");
+            alert.setHeaderText("Zwyciestwo dla " + playerNumber);
+            alert.setContentText("Dziękujemy za wspolną zabawę");
+
+            alert.show();
+        }
 
         public void handleMouseClick(MouseEvent event) {
 
-            pole1.setText("test");
-            if (isOpen() || clickCount == 0)
-                return;
+            System.out.println(gameActive);
+            if (activePlayer == 1) {
+                punkty = pole1;
 
-            clickCount--;
+                czas = pole3;
+                playerNumber = "Gracz 1 ";
 
-            if (selected == null) {
-                selected = this;
-
-                open(() -> {});
             }
             else {
-                open(() -> {
-                    if (!hasSameValue(selected)) {
-                        selected.close();
 
-                        this.close();
-                    }
+                punkty = pole2;
+                czas = pole4;
 
-                    selected = null;
-                    clickCount = 2;
-                });
+                playerNumber = "Gracz 2 ";
+
+            }
+            if ( gameActive == true){
+
+                if (isOpen() || clickCount == 0)
+                    return;
+
+                clickCount--;
+
+                if (selected == null) {
+                    selected = this;
+
+                    open(() -> {
+                    });
+
+                } else {
+                    open(() -> {
+                        if (!hasSameValue(selected)) {
+
+                            selected.close();
+
+                            this.close();
+                        }
+
+                        if (hasSameValue(selected)) {
+                            score++;
+                            System.out.println("Score " + score);
+
+
+
+                            punkty.setText("Punkty " + playerNumber + score);
+                            if (score == Liczba_Par) {
+
+                                timeline.stop();
+                                stopTime = System.nanoTime();
+                                System.out.println(stopTime);
+
+                                timeResult = (stopTime - startTime) / 1_000_000_000;
+
+                                String stringTimeResult = String.format("%.2f", timeResult);
+                                timeResultText.setText("Twój czas to " + stringTimeResult + "s");
+
+                                czas.setText("Czas " + playerNumber + stringTimeResult + "s");
+
+
+                            }
+
+                            if(activePlayer == 1) {
+                                timeResultP1 = timeResult;
+                                System.out.println("TimeResultP1 " + timeResultP1);
+                                scoreP1 = score;
+                                System.out.println("ScoreP1 " + scoreP1);
+
+                            }
+                            else{
+                                timeResultP2 = timeResult;
+                                System.out.println("TimeResultP2 " + timeResultP2);
+                                scoreP2 = score;
+                                System.out.println("ScoreP2 " + scoreP2);
+                            }
+
+                        }
+
+                        selected = null;
+                        clickCount = 2;
+
+
+                        if(scoreP1 > 0  && scoreP2 >0 && timeResultP1 >0  && timeResultP2 > 0){
+                         if(timeResultP1 < timeResultP2){
+                         System.out.println("Gracz 1 wygrał");
+                         playerNumber = "Gracz 1";
+
+                             showAlert();
+                         }
+                         else {
+                         System.out.println("Gracz 2 wygrał");
+                             playerNumber = "Gracz 2";
+                             showAlert();
+                         }
+                         }
+                        if(timeResultP1 == -1 && timeResultP2 == -1 && scoreP1 !=0 && scoreP2 !=0){
+                            if(scoreP1 < scoreP2){
+                                System.out.println("Gracz 2 wygrał");
+                                playerNumber = "Gracz 2";
+
+                                showAlert();
+                            }
+                            if( scoreP1 > scoreP2) {
+                                System.out.println("Gracz 1 wygrał");
+                                playerNumber = "Gracz 1";
+                                showAlert();
+                            }
+                            if (scoreP2 == scoreP1) {
+                                System.out.println("Remis");
+                                playerNumber = "Gracz 1 i Gracz 2 - REMIS";
+                                showAlert();
+                            }
+                        }
+
+
+                    });
+                }
+
+
             }
         }
 
@@ -164,6 +420,7 @@ public class memoAppJavaFx extends Application {
             animacja.setToValue(0); /* wartosc 0 do wygaszenia */
             animacja.play();
         }
+
 
         public boolean hasSameValue(Dachowka other) { /*sprawdzenie czy wartosci puzza sa takie same */
             return text.getText().equals(other.text.getText());
